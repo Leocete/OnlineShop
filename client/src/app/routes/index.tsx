8c875b6373a9cf33676cfce7d712d3b5401408b4
Ivan Shvets <ishvets@mycubes.nl>
2021-04-11 19:26:59 +0300

    Added pre-commit hooks

--- client/src/app/routes/index.tsx
+++ client/src/app/routes/index.tsx
@@ -1,21 +1,15 @@
 import React from 'react';
-import logo from '../../assets/images/logo.svg';
-import '../../styles/App.scss';
+import NavigationBar from '../components/NavigationBar';
 
 const App = (): JSX.Element => {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-                    Learn React
-                </a>
-            </header>
-        </div>
-    );
+  console.log("sdasdsad")
+  return (
+    <div className="app-wrapper">
+      <header>
+        <NavigationBar />
+      </header>
+    </div>
+  );
 };
 
 export default App;
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './styles/App.scss';
+import './styles/app.scss';
 import App from './app/routes';
 
 ReactDOM.render(
