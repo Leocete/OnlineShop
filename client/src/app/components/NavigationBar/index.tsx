38fafe1edf723d40219fe5774267e54f0c47c7f3
Ivan Shvets <ishvets@mycubes.nl>
2021-04-11 19:29:28 +0300

    Test pre-commit hook

--- client/src/app/components/NavigationBar/index.tsx
+++ client/src/app/components/NavigationBar/index.tsx
@@ -3,16 +3,15 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
-//
 
-const NavigationBar = (): JSX.Element => {
-  return (
-    <AppBar position="static" className="nav-bar">
-      <Toolbar>
-        <Typography variant="h3">Zlett</Typography>
-      </Toolbar>
-    </AppBar>
-  );
-};
+    const NavigationBar = (): JSX.Element => {
+      return (
+        <AppBar position="static" className="nav-bar">
+          <Toolbar>
+            <Typography variant="h3">Zlett</Typography>
+          </Toolbar>
+        </AppBar>
+      );
+    };
 
 export default NavigationBar;
